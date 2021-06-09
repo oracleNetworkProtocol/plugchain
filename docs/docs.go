e8bd9d7819e8513214b7130cce0d01f863f09c5d
z2453564902 <2453564902@qq.com>
2021-06-09 15:17:56 +0800

    update route

--- docs/docs.go
+++ docs/docs.go
@@ -6,12 +6,15 @@ import (
 	"net/http"
 )
 
-//go:embed index.tpl
+//go:embed static
 var Docs embed.FS
+
+//go:embed index.tpl
+var index embed.FS
 
 // Handler returns an http handler that servers OpenAPI console for an OpenAPI spec at specURL.
 func Handler(title, specURL string) http.HandlerFunc {
-	t, err := template.ParseFS(Docs, "index.tpl")
+	t, err := template.ParseFS(index, "index.tpl")
 	if err != nil {
 		panic(err)
 	}
