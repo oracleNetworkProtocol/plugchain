36a90e16fdc5af8747e6a3f15d8714b278fa252f
z2453564902 <2453564902@qq.com>
2021-07-07 09:14:58 +0800

    add setup.sh

---
+++ setup-1.sh
@@ -0,0 +1,14 @@
+APPNAME=node/node1
+CHAINID=chain-1
+DEMON=aaa
+DAEMON=plugchaind
+rm -rf $APPNAME
+$DAEMON init node1 --home $APPNAME --chain-id $CHAINID
+sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:26657#g' $APPNAME/config/config.toml
+sed -i 's#stake#aaa#g' $APPNAME/config/genesis.json
+sed -i 's/pruning = "default"/pruning = "nothing"/g' $APPNAME/config/app.toml
+$DAEMON keys add jcl --home $APPNAME
+$DAEMON add-genesis-account jcl "200000000000000 $DEMON" --home $APPNAME
+$DAEMON gentx jcl "20000000000$DEMON" --min-self-delegation "1000000" --home $APPNAME --chain-id $CHAINID
+$DAEMON collect-gentxs --home $APPNAME
+$DAEMON start --home $APPNAME
--- setup-2.sh
+++ setup-2.sh
@@ -1,10 +1,12 @@
-APPNAME=node/node1
-CHAINID=test-plugchain
-DEMON=line
+APPNAME=node/node2
+CHAINID=chain-2
+DEMON=bbb
 DAEMON=plugchaind
 rm -rf $APPNAME
 $DAEMON init node1 --home $APPNAME --chain-id $CHAINID
-sed -i "" 's#stake#line#g' $APPNAME/config/genesis.json
+sed -i 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:26657#g' $APPNAME/config/config.toml
+sed -i 's#stake#bbb#g' $APPNAME/config/genesis.json
+sed -i 's/pruning = "default"/pruning = "nothing"/g' $APPNAME/config/app.toml
 $DAEMON keys add jcl --home $APPNAME
 $DAEMON add-genesis-account jcl "20000000000000 $DEMON" --home $APPNAME
 $DAEMON gentx jcl "20000000000$DEMON" --min-self-delegation "1000000" --home $APPNAME --chain-id $CHAINID
