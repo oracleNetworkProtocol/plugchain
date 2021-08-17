ef18e8226f251e64a7fc5d91fa0139fb2141f813
配角on <1171119472@qq.com>
2021-08-17 10:50:15 +0800

    node

--- node.sh
+++ node.sh
@@ -3,22 +3,27 @@ make install
 rm -rf home1
 NODENAME1="home1"
 
-CHAIN_ID=chain-a
-PASSWORD="123123123"
-DENOM=onp
+CHAIN_ID=plugchain
+
+DENOM=plug
 plugchaind init $NODENAME1 --chain-id $CHAIN_ID --home $NODENAME1
 
-(echo $PASSWORD;echo $PASSWORD) | plugchaind keys add mywallet  --home $NODENAME1 
-(echo $PASSWORD;echo $PASSWORD) | plugchaind keys add mywallet1  --home $NODENAME1 
+plugchaind keys add mywallet  --home $NODENAME1 
+plugchaind keys add mywallet1  --home $NODENAME1 
 
 sed -i '' 's#tcp://127.0.0.1:26657#tcp://0.0.0.0:26657#g' "$(pwd)/"$NODENAME1/config/config.toml
 sed -i '' "s/\"stake\"/\"$DENOM\"/g" "$(pwd)/"$NODENAME1/config/genesis.json
 sed -i '' 's/pruning = "syncable"/pruning = "nothing"/g' "$(pwd)/"$NODENAME1/config/app.toml
 
-echo $PASSWORD | plugchaind add-genesis-account $(plugchaind keys show mywallet -a --home $NODENAME1)  "1000000000 onp,10000000 plug" --home $NODENAME1 &&\
-echo $PASSWORD | plugchaind add-genesis-account $(plugchaind keys show mywallet1 -a --home $NODENAME1) "1000000000 onp,10000000 plug" --home $NODENAME1
+plugchaind add-genesis-account $(plugchaind keys show mywallet -a --home $NODENAME1)  "15869000000000000 plug" --home $NODENAME1 &&\
+plugchaind add-genesis-account $(plugchaind keys show mywallet1 -a --home $NODENAME1) "80000000000000 plug" --home $NODENAME1 &&\
+plugchaind add-genesis-account $(plugchaind keys show mywallet2 -a --home $NODENAME1) "20000000000000 plug" --home $NODENAME1 &&\
+plugchaind add-genesis-account $(plugchaind keys show mywallet3 -a --home $NODENAME1) "20000000000000 plug" --home $NODENAME1 &&\
 
-echo $PASSWORD | plugchaind gentx mywallet 100000000onp --keyring-dir $NODENAME1 --home $NODENAME1 --chain-id $CHAIN_ID
+plugchaind gentx mywallet 80000000000000plug --keyring-dir $NODENAME1 --home $NODENAME1 --chain-id $CHAIN_ID &&\
+plugchaind gentx mywallet 80000000000000plug --keyring-dir $NODENAME1 --home $NODENAME1 --chain-id $CHAIN_ID &&\
+plugchaind gentx mywallet 20000000000000plug --keyring-dir $NODENAME1 --home $NODENAME1 --chain-id $CHAIN_ID &&\
+plugchaind gentx mywallet 20000000000000plug --keyring-dir $NODENAME1 --home $NODENAME1 --chain-id $CHAIN_ID &&\
 
 plugchaind collect-gentxs --home $NODENAME1
 
@@ -28,4 +33,3 @@ plugchaind collect-gentxs --home $NODENAME1
 # echo $(jq . $(pwd)/$NODENAME1/config/gentx/*.json)
 # echo $(jq . $(pwd)/$NODENAME2/config/gentx/*.json)
 # echo $(jq . $(pwd)/$NODENAME3/config/gentx/*.json)
-plugchaind start --home home1  
