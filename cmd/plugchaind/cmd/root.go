fef595955df463dc17f94e1b8bf66a7965849691
worryFree56 <z2453564902@gmail.com>
2021-12-09 09:54:54 +0800

    dump ethermint for v0.7.2

--- cmd/plugchaind/cmd/root.go
+++ cmd/plugchaind/cmd/root.go
@@ -56,12 +56,16 @@ func NewRootCmd() (*cobra.Command, app.EncodingConfig) {
 		Use:   app.Name + "d",
 		Short: "CosmosHub PlugChain App",
 		PersistentPreRunE: func(cmd *cobra.Command, _ []string) error {
-			initClientCtx = client.ReadHomeFlag(initClientCtx, cmd)
-			initClientCtx, err := config.ReadFromClientConfig(initClientCtx)
 
+			initClientCtx, err := client.ReadPersistentCommandFlags(initClientCtx, cmd.Flags())
 			if err != nil {
 				return err
 			}
+			initClientCtx, err = config.ReadFromClientConfig(initClientCtx)
+			if err != nil {
+				return err
+			}
+
 			if err = client.SetCmdClientContextHandler(initClientCtx, cmd); err != nil {
 				return err
 			}
