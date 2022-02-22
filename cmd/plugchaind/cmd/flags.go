95ffb6d024595aab84586e865396c6237ff4b4a3
worryFree56 <z2453564902@gmail.com>
2022-02-22 15:58:13 +0800

    fix flags to node and keyringbackend not found

--- cmd/plugchaind/cmd/flags.go
+++ cmd/plugchaind/cmd/flags.go
@@ -25,11 +25,11 @@ func AddTxFlags(cmd *cobra.Command) (*cobra.Command, error) {
 	// ))
 
 	// viper.BindPFlag(flags.FlagTrustNode, cmd.Flags().Lookup(flags.FlagTrustNode))
-	if err := viper.BindPFlag(flags.FlagNode, cmd.Flags().Lookup(flags.FlagNode)); err != nil {
+	if err := viper.BindPFlag(flags.FlagNode, cmd.PersistentFlags().Lookup(flags.FlagNode)); err != nil {
 		return nil, err
 	}
 	// nolint: errcheck
-	if err := viper.BindPFlag(flags.FlagKeyringBackend, cmd.Flags().Lookup(flags.FlagKeyringBackend)); err != nil {
+	if err := viper.BindPFlag(flags.FlagKeyringBackend, cmd.PersistentFlags().Lookup(flags.FlagKeyringBackend)); err != nil {
 		return nil, err
 	}
 	return cmd, nil
