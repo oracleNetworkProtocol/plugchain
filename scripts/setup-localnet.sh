f5596b8c26795917deb1206eb5ee603f5adbdc75
worryFree56 <z2453564902@gmail.com>
2022-06-23 16:42:43 +0800

    setup-local add val-adr

--- scripts/setup-localnet.sh
+++ scripts/setup-localnet.sh
@@ -20,8 +20,9 @@ rm -rf $NODEDIR
 plugchaind config keyring-backend $KEYRING --home $NODEDIR
 plugchaind config chain-id $CHAINID --home $NODEDIR
 
-# if $KEY exists it should be deleted
-plugchaind keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --home $NODEDIR
+VAL_MNEMONIC="ticket used error wait wave wait top what fix garlic jewel body frozen tone reject tomorrow voyage album aware shop safe trumpet slush skin"
+
+echo $VAL_MNEMONIC   | plugchaind keys add $KEY --keyring-backend $KEYRING --algo $KEYALGO --recover --home $NODEDIR
 
 # Set moniker and chain-id for Evmos (Moniker can be anything, chain-id must be an integer)
 plugchaind init $MONIKER --chain-id $CHAINID --home $NODEDIR
@@ -32,6 +33,7 @@ cat $NODEDIR/config/genesis.json | jq '.app_state["crisis"]["constant_fee"]["den
 cat $NODEDIR/config/genesis.json | jq '.app_state["gov"]["deposit_params"]["min_deposit"][0]["denom"]="uplugcn"' > $NODEDIR/config/tmp_genesis.json && mv $NODEDIR/config/tmp_genesis.json $NODEDIR/config/genesis.json
 cat $NODEDIR/config/genesis.json | jq '.app_state["mint"]["params"]["mint_denom"]="uplugcn"' > $NODEDIR/config/tmp_genesis.json && mv $NODEDIR/config/tmp_genesis.json $NODEDIR/config/genesis.json
 cat $NODEDIR/config/genesis.json | jq '.app_state["evm"]["params"]["evm_denom"]="uplugcn"' > $NODEDIR/config/tmp_genesis.json && mv $NODEDIR/config/tmp_genesis.json $NODEDIR/config/genesis.json
+cat $NODEDIR/config/genesis.json | jq '.app_state["feemarket"]["params"]["base_fee"]="7"' > $NODEDIR/config/tmp_genesis.json && mv $NODEDIR/config/tmp_genesis.json $NODEDIR/config/genesis.json
 cat $NODEDIR/config/genesis.json | jq '.app_state["liquidity"]["params"]["pool_creation_fee"][0]["denom"]="uplugcn"' > $NODEDIR/config/tmp_genesis.json && mv $NODEDIR/config/tmp_genesis.json $NODEDIR/config/genesis.json
 
 # increase block time (?)
@@ -90,4 +92,4 @@ if [[ $1 == "pending" ]]; then
 fi
 
 # Start the node (remove the --pruning=nothing flag if historical queries are not needed)
-plugchaind start --pruning=nothing $TRACE --log_level $LOGLEVEL --minimum-gas-prices=0.0001uplugcn --json-rpc.api eth,txpool,personal,net,debug,web3 --home $NODEDIR
+# plugchaind start --pruning=nothing $TRACE --evm.max-tx-gas-wanted 20000 --log_level $LOGLEVEL --minimum-gas-prices=0.0001uplugcn --json-rpc.api eth,txpool,personal,net,debug,web3 --home $NODEDIR
