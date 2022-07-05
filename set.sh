70ce426a5b126787b385a09f5ad829de77756b7e
worryFree56 <z2453564902@gmail.com>
2022-07-05 15:55:27 +0800

    ibc

---
+++ set.sh
@@ -0,0 +1,11 @@
+rm -rf /Users/jcl/.relayer
+rly config init
+rly config add-chains ibc/chains
+
+ibcplug="bottom dad own clap trade dice action ripple crater obvious mountain cement penalty first doctor advice keen reform coyote fun leader verb december dry"
+ibconp="kick oyster love green crowd culture course zoo wood female shed forward track small urban flame imitate analyst combine protect beauty blur pear donate"
+
+rly keys restore plugchain_520-1 ibcplug "$ibcplug"
+rly keys restore onp_521-1 ibconp "$ibconp"
+rly config add-paths ibc/paths
+rly start ibc-plug
